1b3489339fb652c0cb35259fed207664520fac35
Nick <nikita.repenko@gmail.com>
2020-07-21 20:06:33 +0300

    second lesson

---
+++ js/02.js
@@ -0,0 +1,41 @@
+'use strict';
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели', '');
+    
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+
+    },
+    actors: {
+
+    },
+    genres: [],
+    privat: false
+}
+
+for(let i = 0; i < 2; i++){
+    let a;
+    let b;
+    a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
+
+    if(a != null && b!= null && a!= '' && b!= '' && a!= a.length < 50){
+        personalMovieDB.movies[a] = b;
+    }else{
+        i--;
+    }
+};
+
+if( personalMovieDB.count < 10){
+    alert('Просмотрено довольно мало фильмов');
+}else if(personalMovieDB.count > 10 && personalMovieDB.count < 30){
+    alert('Вы классический зритель');
+}
+else if(personalMovieDB.count > 30){
+    alert('Вы киноман');
+}else{
+    alert('Произошла ошибка')
+}
+
+console.log(personalMovieDB)
+
